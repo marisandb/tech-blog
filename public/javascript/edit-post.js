f0cece498c7db3a3e602ca5000f9e197ebfeb25f
maris <marisandb@yahoo.com>
2021-09-14 22:40:34 -0500

    created mvp

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,28 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+  
+    const postData = {
+      title: document.querySelector('input[name="post-title"]').value.trim(), 
+      post_contents: document.getElementById('post-contents').value.trim(),
+    }
+    const id = window.location.toString().split("/")[
+      window.location.toString().split("/").length - 1
+    ];
+    const response = await fetch(`/api/posts/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    if (response.ok) {
+      document.location.replace("/dashboard/");
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document
+    .querySelector(".edit-post-form")
+    .addEventListener("submit", editFormHandler);
