cf685af5fff8bad333827493c3543e663451c206
maris <marisandb@yahoo.com>
2021-09-17 23:34:17 -0500

    fix bug

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//   secret: process.env.DB_SECRET,
-//   cookie: {
-//     expires: 600000
-//   },
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: process.env.DB_SECRET,
+  cookie: {
+    expires: 600000
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -30,7 +30,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-//app.use(session(sess));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
