7c68c452d819575a152978cf370798a020def339
maris <marisandb@yahoo.com>
2021-09-17 22:53:28 -0500

    attempt to fix heroku issue

--- server.js
+++ server.js
@@ -1,42 +1,41 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const helpers = require('./utils/helpers')
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const sequelize = require("./config/connection");
+const hbs = exphbs.create({ helpers });
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.DB_SECRET,
-    cookie: {
-      expires: 600000
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
-  
-
-app.use(session(sess));
-
-const helpers = require('./utils/helpers');
-
-const hbs = exphbs.create({ helpers });
+  secret: process.env.DB_SECRET,
+  cookie: {
+    expires: 600000
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
 
-app.use(require('./controllers/'));
+// turn on routes
+app.use(routes);
 
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
